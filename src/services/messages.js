c79ee1dc64a6e365ee9b99d5ab13363f621b246b
Anton <antony.it.work@gmail.com>
2017-09-10 22:06:03 +0300

    in service added function sending a message to the database

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,10 +1,18 @@
 export function getMessages() {
-  return fetch('http://localhost:3000/messages', {}).then(res => res.json().then(data => data.data[0].messages[0]));
+  return fetch('http://localhost:3000/messages', {}).then(res => res.json().then(data => data.data[0].messages));
 }
 
-export function createNewChat(data) {
+export function sendMessage(message) {
+  const user = JSON.parse(localStorage.getItem('userData') || '{}');
+  const time = new Date();
+  const sendTime = `${time.getHours()}:${time.getMinutes()}`
+  const data = {
+    whoSend: user.firstname,
+    text: message.message,
+    sendTime: sendTime,
+  };
   return (
-    fetch('http://localhost:3000/createChat', {
+    fetch('http://localhost:3000/newMessage', {
       headers: {
         'Content-Type': 'application/json',
       },
