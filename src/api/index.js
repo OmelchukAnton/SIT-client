824c551b90a6c48782c30c69030c7b33df5aa113
Anton <antony.it.work@gmail.com>
2017-11-02 23:08:49 +0300

    change endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
-const apiEndpoint = 'http://localhost:5000';
-// const apiEndpoint = 'https://glacial-basin-12371.herokuapp.com';
+const apiEndpoint = 'https://glacial-basin-12371.herokuapp.com';
 
 
 export function get(url) {
